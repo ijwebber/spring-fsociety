2f842f5e5ac01de9776d6cbdf23529480e128b9c
ijwebber <isaacjwebber@gmail.com>
2019-11-02 22:58:58 +0000

    made the form page

---
+++ src/main/java/com/fsociety/controllers/FsocietyController.java
@@ -0,0 +1,31 @@
+package com.fsociety.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class FsocietyController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloForm() {
+        return "form";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String index(HttpServletRequest request, Model model) {
+
+        //get from the ? part of the url
+        String name = request.getParameter("name");
+
+        if (name == null || name == "") {
+            name = "world";
+        }
+
+        model.addAttribute("message", name);
+        return "welcome";
+    }
+}
